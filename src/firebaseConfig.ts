aaf5603b3814d323fc3d83591e61d8fce74a61cc
Unaihub <david.howe@Unaihub.com>
2023-04-21 02:19:58 -0700

    Add landing page components and styles

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCfc5iHdTUSZ0MqpBUdpRMxmyYxoLHlnSM",
+  authDomain: "unaihub-37646.firebaseapp.com",
+  projectId: "unaihub-37646",
+  storageBucket: "unaihub-37646.appspot.com",
+  messagingSenderId: "1083483403475",
+  appId: "1:1083483403475:web:39bf656564a43b714c1026",
+  measurementId: "G-8RLZ3DZGK9",
+};

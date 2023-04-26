980ca859a2452a178bf99fcef495deb876ce8420
Unaihub <david.howe@Unaihub.com>
2023-04-26 01:32:43 -0700

    git commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import Link from "next/link";
 import Layout from "../components/Layout";
 
+// Import the AnimatedTextGrid component
+import AnimatedTextGrid from "../components/AnimatedTextGrid";
+
 const IndexPage = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <header>
         <canvas></canvas>
       </header>
+      {/* Add the AnimatedTextGrid component */}
+      <AnimatedTextGrid text="UNAIHUB" />
       <h1>Hello Next.js 👋</h1>
       <p>
         <Link href="/about">About</Link>
@@ -16,4 +21,6 @@ const IndexPage = () => {
   );
 };
 
-export default IndexPage;
+const AnimatedTextGrid: React.FC<AnimatedTextGridProps> = ({ ... }) => { ... }
+export default AnimatedTextGrid;
+

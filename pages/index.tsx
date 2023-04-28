85baf9da39d8e1bd84ea4d751a62c1121722fa83
Unaihub <david.howe@Unaihub.com>
2023-04-27 22:36:04 -0700

    git commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,32 @@
+// pages/index.tsx
 import React from "react";
 import Link from "next/link";
 import Layout from "../components/Layout";
-
-// Import the AnimatedTextGrid component
 import AnimatedTextGrid from "../src/components/AnimatedTextGrid";
 
 const IndexPage = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
+      {/* The header containing the canvas */}
       <header>
         <canvas></canvas>
       </header>
-      {/* Add the AnimatedTextGrid component */}
+
+      {/* Add the AnimatedTextGrid component, which will be centered and cover the entire page */}
       <AnimatedTextGrid text="UNAIHUB" />
-      <h1>Hello Next.js 👋</h1>
-      <p>
-        <Link href="/about">About</Link>
-      </p>
+
+      {/* 
+        Wrap the other content in a separate div and set its position to "relative" 
+        and zIndex to 0 to ensure it's placed below the AnimatedTextGrid component.
+        If you want this content to be visible after the animation, you can modify the
+        zIndex and background-color properties of the AnimatedTextGrid component accordingly.
+      */}
+      <div style={{ position: "relative", zIndex: 0 }}>
+        <h1>Hello Next.js 👋</h1>
+        <p>
+          <Link href="/about">About</Link>
+        </p>
+      </div>
     </Layout>
   );
 };

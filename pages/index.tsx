12b8c95689e8ae378a62d1aae4c6ce4a62d29ee8
Unaihub <david.howe@Unaihub.com>
2023-04-29 01:06:30 -0700

    git commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
-// Import required modules and components
 import React from "react";
 import Head from "next/head";
 import AnimatedTextGrid from "../src/components/AnimatedTextGrid";
 import styles from "../styles/AnimatedTextGrid.module.scss";
 
-// IndexPage component
+// Constants for app name and slogan
+const appName = "UNAIHUB";
+const slogan = "Your AI Hub";
+
+// IndexPage component for the landing page
 const IndexPage = () => {
   return (
     <>
       {/* 
-        Use the Head component to update the title of the page and set the viewport
+        The Head component is used to update the title of the page and set the viewport
         for responsive design
       */}
       <Head>
@@ -18,11 +21,15 @@ const IndexPage = () => {
       </Head>
 
       {/* 
-        Wrap the AnimatedTextGrid component in a div with the styles.animatedTextGrid
+        The AnimatedTextGrid component is wrapped in a div with the styles.animatedTextGrid
         className to apply the correct styling to the landing page
       */}
       <div className={styles.animatedTextGrid}>
-        <AnimatedTextGrid text="UNAIHUB" />
+        {/* 
+          Pass the combined app name and slogan as the text prop for the AnimatedTextGrid
+          component to display the animated text
+        */}
+        <AnimatedTextGrid text={`${appName} - ${slogan}`} />
       </div>
     </>
   );
